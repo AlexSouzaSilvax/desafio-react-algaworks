9485e9c66101e38c8ce191c3a7818c34a3e03870
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2020-09-19 10:11:06 -0300

    base layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import AppContainer from "../AppContainer/AppContainer";
+import AppHeader from "../AppHeader";
 import { Container, Wrapper } from "./App.styles";
 
 function App() {
   return (
-    <Wrapper>      
-      <Container />
+    <Wrapper>
+      <Container>
+        <AppHeader />
+        <AppContainer
+          left={<div style={{backgroundColor: 'red'}}>produtos disponíveis</div>}
+          middle={<div style={{backgroundColor: 'green'}}>sua lista de compras</div>}
+          right={<div style={{backgroundColor: 'blue'}}>estatísticas</div>}
+        />
+      </Container>
     </Wrapper>
   );
 }
---
+++ src/components/AppHeader/AppHeader.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Wrapper } from "./AppHeader.styles";
+
+function AppHeader() {
+  return <Wrapper>alga <span>Shopping</span></Wrapper>;
+}
+
+export default AppHeader;
