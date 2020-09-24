ffe2abc99152e18d21efe2fa145efc0a853f193b
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2020-09-24 11:24:58 -0300

    implement redux-persist

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import HomeView from "./views/Home.view";
-import store from "./store/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persisedStore } from "./store/store";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HomeView />
+      <PersistGate persistor={persisedStore}>
+        <HomeView />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
